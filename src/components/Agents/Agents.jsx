095f8c8fbd3a3a0decddee8a2cf6382132f05c0b
Vinay Maurya <Vinay34978@gmail.com>
2025-05-02 16:13:55 +0530

    Updated ngrok url

--- src/components/Agents/Agents.jsx
+++ src/components/Agents/Agents.jsx
@@ -423,7 +423,7 @@ const SupplyChainView = ({ onBack }) => {
             </div>
           )}
           <iframe
-            src=" https://da75-183-82-119-26.ngrok-free.app"
+            src="https://efd7-183-82-119-26.ngrok-free.app"
             width="100%"
             height="300px"
             style={{ 
